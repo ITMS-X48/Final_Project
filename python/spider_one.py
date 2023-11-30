415982c657b37338cbf13bca240e9b26d1749aec
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2023-11-30 16:23:57 -0600

    moved datasets and implemented correct spider code

--- python/spider_one.py
+++ python/spider_one.py
@@ -1,11 +1,18 @@
 import scrapy
+import csv
 
 class spider_one(scrapy.spider):
     name = 'uno'
-    start_url = [
-        ''
-    ]
+    def start_requests(self):
+        with open(python/datasets/benign_list_big_final.csv, 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for row in reader:
+                url = row[0]
+                yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        title = response.css(title).extract()
-        yield('title_text': title)
+            title = response.css('title::text').get()
+            meta = response.css('meta[name="description"]::attr(content)').get()
+            item = YourItem(parent_url=response.url)
+            item['child_urls'] = response.css('a::attr(href)').extract()
