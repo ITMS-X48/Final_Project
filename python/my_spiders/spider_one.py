e6ea2c13a9d8d1119267fdb554ba62d4c67acf55
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2023-12-01 15:41:11 -0600

    included documentation how its supposed to work

--- python/my_spiders/spider_one.py
+++ python/my_spiders/spider_one.py
@@ -7,13 +7,13 @@ import os
 import re
 
 class spider_one(scrapy.Spider):
-## First Read through our dataset and select the url's
+## These are the established variables for naming and ip holding/output as well as a max number of ips
     name = 'spider_one'
     scraped_ips = []
     settings = {
         'CLOSESPIDER_ITEMCOUNT': 50,
     }
-
+## This is going to read through the csv file and then fill an order for each url element contained within the file
     def start_requests(self):
         csv_file_path = os.path.abspath('python/my_spiders/datasets/benign_list_big_final.csv')
         with open(csv_file_path, 'r') as file:
@@ -23,21 +23,27 @@ class spider_one(scrapy.Spider):
                 url = row[0]
                 yield scrapy.Request(url=url, callback=self.parse)
 
+
+## Parsing function
     def parse(self, response):
+## This checks to see if the site is responding or not
         if not response.body:
             self.logger.warning('Empty response: %s', response.url)
             return
 
-        # Check if the response is JSON
+# This checks if the response is JSON
         content_type = response.headers.get(b'Content-Type', b'').decode('utf-8').lower()
         if b'application/json' in content_type:
+## Checks if the site is blocking the scraper or if the scraper does not have access to the site for whatever reason
             if response.status == 200:
                 try:
                     data = json.loads(response.body)
+## Checks to see if the JSON response has key = origin and if the value corresponding to the origin is a valid ipv4  address
                     if 'origin' in data and re.match(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', data['origin']):
                         ip_address = data['origin']
                         self.logger.info('IP Address: %s', ip_address)
                         self.scraped_ips.append(ip_address)
+## Hardcoded limit
                         if len(self.scraped_ips) >= self.settings.get('CLOSESPIDER_ITEMCOUNT'):
                             self.crawler.engine.close_spider(self, 'item_count_reached')
                 except json.JSONDecodeError as e:
@@ -45,14 +51,15 @@ class spider_one(scrapy.Spider):
         else:
             self.logger.warning('Response is not JSON: %s', response.url)
 
+##process to stop a spider suddenly
     def stop_spider(self):
         self.logger.info('stopping spider...')
         self.closeSpider('Terminated')
-    
+## part of the process of correctly closing the spider out
     def closeSpider(self, spider, reason):
         self.logger.info('Spider closed: %s', reason)
         self.process_ips()
-    
+## processes the ips so that they can be made use of
     def process_ips(self): 
         self.logger.info('Scraped IPs:%s', self.scraped_ips)
         for x in self.scraped_ips:
