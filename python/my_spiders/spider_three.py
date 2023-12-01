3256eb19e986070c605300e048405ff557633d50
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2023-12-01 12:03:39 -0600

    fixed spider 3

--- python/my_spiders/spider_three.py
+++ python/my_spiders/spider_three.py
@@ -20,6 +20,7 @@ class spider_one(scrapy.Spider):
         ip_address = response.json().get('origin')
         return ip_address
 
+
 def run_spider_three(self):
     process = CrawlerProcess(get_project_settings())
     process.crawl(self)
