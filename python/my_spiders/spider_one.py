e576e154950fc13f378c5f26224696a198c54794
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2023-12-01 16:25:26 -0600

    debugging

--- python/my_spiders/spider_one.py
+++ python/my_spiders/spider_one.py
@@ -1,19 +1,21 @@
 import csv
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-import scrapy
-import json
 import os
 import re
+import json
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scrapy.utils.project import get_project_settings
+from twisted.internet.error import DNSLookupError
 
 class spider_one(scrapy.Spider):
-## These are the established variables for naming and ip holding/output as well as a max number of ips
     name = 'spider_one'
     scraped_ips = []
-    settings = {
-        'CLOSESPIDER_ITEMCOUNT': 50,
+    http_bad_status_list = [301, 302, 400, 401, 402, 403, 404, 410]
+
+    custom_settings = {
+        'CLOSESPIDER_ITEMCOUNT': 5,
     }
-## This is going to read through the csv file and then fill an order for each url element contained within the file
+
     def start_requests(self):
         csv_file_path = os.path.abspath('python/my_spiders/datasets/benign_list_big_final.csv')
         with open(csv_file_path, 'r') as file:
@@ -23,48 +25,52 @@ class spider_one(scrapy.Spider):
                 url = row[0]
                 yield scrapy.Request(url=url, callback=self.parse)
 
-
-## Parsing function
     def parse(self, response):
-## This checks to see if the site is responding or not
-        if not response.body:
-            self.logger.warning('Empty response: %s', response.url)
-            return
+## This try handles DNS lookup errors
+        try:
+## Checks for no response from site
+            if not response.body:
+                self.logger.warning('Empty response: %s', response.url)
 
-# This checks if the response is JSON
-        content_type = response.headers.get(b'Content-Type', b'').decode('utf-8').lower()
-        if b'application/json' in content_type:
-## Checks if the site is blocking the scraper or if the scraper does not have access to the site for whatever reason
-            if response.status == 200:
-                try:
-                    data = json.loads(response.body)
-## Checks to see if the JSON response has key = origin and if the value corresponding to the origin is a valid ipv4  address
-                    if 'origin' in data and re.match(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', data['origin']):
-                        ip_address = data['origin']
-                        self.logger.info('IP Address: %s', ip_address)
-                        self.scraped_ips.append(ip_address)
-## Hardcoded limit
-                        if len(self.scraped_ips) >= self.settings.get('CLOSESPIDER_ITEMCOUNT'):
-                            self.crawler.engine.close_spider(self, 'item_count_reached')
-                except json.JSONDecodeError as e:
-                    self.logger.error('Error decoding JSON: %s', str(e))
-        else:
-            self.logger.warning('Response is not JSON: %s', response.url)
+            content_type = response.headers.get(b'Content-Type', b'').decode('utf-8').lower()
+## Makes sure it's a JSON response we're dealing with here
+            if 'application/json' in content_type:
+## If the http response is OK
+                if response.status == 200:
+## Try unless there's an issue with the JSON
+                    try:
+                        data = json.loads(response.body)
+## Makes sure that the JSON responds with an origin key that is a proper ipv4 address
+                        if 'origin' in data and re.match(r'\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b', data['origin']):
+                            ip_address = data['origin']
+                            self.logger.info('IP Address: %s', ip_address)
+                            self.scraped_ips.append(ip_address)
+## Hardcoded stop to check only so many ips
+                            if len(self.scraped_ips) >= self.custom_settings['CLOSESPIDER_ITEMCOUNT']:
+                                self.crawler.engine.close_spider(self, 'item_count_reached')
+                    except json.JSONDecodeError as e:
+                        self.logger.error('Error decoding JSON: %s', str(e))
+                elif response.status in self.http_bad_status_list:
+                    self.logger.warning(f"Ignoring Bad HTTP Error: {response.status}")
+            else:
+                self.logger.warning('Response is not JSON: %s', response.url)
+        except DNSLookupError as e:
+            self.logger.error(f"DNS lookup failed: {response.url}: {str(e)}")
 
-##process to stop a spider suddenly
+## these are for stopping the spider and making sure they output the correct ips
     def stop_spider(self):
-        self.logger.info('stopping spider...')
-        self.closeSpider('Terminated')
-## part of the process of correctly closing the spider out
-    def closeSpider(self, spider, reason):
+        self.logger.info('Stopping spider...')
+        self.close_spider('Terminated')
+
+    def close_spider(self, reason):
         self.logger.info('Spider closed: %s', reason)
         self.process_ips()
-## processes the ips so that they can be made use of
-    def process_ips(self): 
+
+    def process_ips(self):
         self.logger.info('Scraped IPs:%s', self.scraped_ips)
         for x in self.scraped_ips:
             print(x)
-    
+
 def run_spider_one():
     process = CrawlerProcess(get_project_settings())
     process.crawl(spider_one)
