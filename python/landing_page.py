13901b0716af4dbe1852d678092c6d736f3756b4
Daniela Munoz <dmunoz9@hawk.iit.edu>
2023-12-01 08:42:24 -0600

    gui-capabilities

--- python/landing_page.py
+++ python/landing_page.py
@@ -3,13 +3,21 @@
 
 from tkinter import *
 import tkinter as tk
+from tkinter import ttk
+import scrapy
+from my_spiders import spider_one
+from my_spiders import spider_two
+from my_spiders import spider_three
+import json
+import requests
+import webbrowser
 
 window = tk.Tk()
 window.geometry("900x700")
-window.title("Dummy Name")
+window.title("IMTS-X48")
 
 #intro
-label = tk.Label(window, text="Welcome to Dummy Name", font=('Courier 22 bold'))
+label = tk.Label(window, text="Welcome to Sneaky Web Crawler", font=('Courier 22 bold'))
 label.pack(padx=20, pady=20)
 
 #how to use label
@@ -22,25 +30,57 @@ how_to_info = "Enter a website URL in the target box below to run the crawler. E
 how_to_use = tk.Label(window, text=how_to_info, font=('Courier', 12), justify=tk.LEFT, wraplength=window.winfo_screenwidth() - 40)
 how_to_use.pack(padx=20, pady=20)
 
-#target label for user input
-target = Label(window, text="Target:", font = ('Courier', 15))
-target.pack()
+#dropdown box for target selection
+def show():
+    label.config( text = clicked.get() )
 
-#accepting target input
-input = Entry(window, width = 40)
-input.focus_set()
-input.pack()
+#benign= spider 1, malware= spider 2, phishing= spider 3
+options = ["Benign List", "Malware Dataset", "Phishing Dataset"]
+
+clicked = StringVar()
+clicked.set ("Benign List")
+
+drop = OptionMenu( window, clicked, *options )
+drop.pack()
+
+target = Button( window, text = "Select Target", command = show ).pack()
+
+menu = Label(window, text = " ")
+menu.pack()
 
 #button to validate
-tk.Button(window, text = "Go Crawl", width = 20).pack(pady=20)
+tk.Button(window, text = "Go Crawl", command = show, width = 20).pack(pady=20)
 
-#output (in same page)
+#running the spider
+def on_crawl_clicked():
+    selected_option = clicked.get()
+    if selected_option == "Benign List":
+        spider_one.run_spider_one()
+        print("Checking for success")
+    elif selected_option == "Malware Dataset":
+        spider_two.run_spider_two()
+        print("Checking for success")
+    elif selected_option == "Phishing Dataset":
+        spider_three.run_spider_three()
+        print("Checking for success")
+    else:
+        print("Please select a spider")
+        return
 
-#analysis (in same page)
 
-#output portion
+# from url in list, pull the ip, append to the api url, pull all json info
+# Pulling JSON info
+
+
 
 #report button ("Would you like to report this malicious page to Google? | -> send them to googles report page")
+def link():
+    url = "https://developers.google.com/search/help/report-quality-issues" 
+    webbrowser.open(url)
+
+report_button = Button(window, text = "Report Malicious Page", command=link)
+report_button.pack(pady=20)
+
 
 #pdf button ("Copy of Report")
 
