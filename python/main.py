aabd1d2c8175be7422d96b41f0fa673439010213
Joe-Valente <112267629+Joe-Valente@users.noreply.github.com>
2023-12-01 01:16:44 -0600

    fixed spider call

--- python/main.py
+++ python/main.py
@@ -1,7 +1,20 @@
-from my_spiders import spider_one
+from my_spiders import spider_one, spider_two, spider_three
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 
 def run_spider_one():
     process = CrawlerProcess(get_project_settings())
     process.crawl(spider_one)
+    process.start()
+
+def run_spider_two():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(spider_two)
+    process.start()
+
+def run_spider_three():
+    process = CrawlerProcess(get_project_settings())
+    process.crawl(spider_three)
+    process.start()
+
+## Implement GUI here
